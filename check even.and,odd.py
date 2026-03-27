1eb7ecddf23754be6bf6ee35a6695b7ff6beaee3
diwakar chaudhary <boyroyal4853@gmail.com>
2026-03-27 19:35:51 +0530

    add

--- check even.and,odd.py
+++ check even.and,odd.py
@@ -1,21 +1,23 @@
 def check_even_odd(number):
     if number % 2 == 0:
         return f"{number} is Even"
+        # print("even number")
     else:
         return f"{number} is Odd"
+        # print("odd number")
 
 #  Example
 print(check_even_odd(10))
-print(check_even_odd(7))
+# print(check_even_odd(7))
 
-# check_even_odd = lambda x = 89: "Even" if x % 2 == 0 else "Odd"
+# # check_even_odd = lambda x = 89: "Even" if x % 2 == 0 else "Odd"
 
-# Example
-print(check_even_odd(12))  # Even
-print(check_even_odd(9))   # Odd
+# # Example
+# print(check_even_odd(12))  # Even
+# print(check_even_odd(9))   # Odd
 
-print(check_even_odd(10))  
-print(check_even_odd(8))  
+# print(check_even_odd(10))  
+# print(check_even_odd(8))  
  
-print(check_even_odd(9))   
-print(check_even_odd(80))   
+# print(check_even_odd(9))   
+# print(check_even_odd(80))   
--- chek _code.py
+++ chek _code.py
@@ -1,7 +1,7 @@
-# # my_listt = [11,33,35,34]
-# # print("before any operation", my_listt)
-# # my_listt.sort()  # it sorts the list in ascending order.
-# # print("after operation", my_listt)
+# my_listt = [11,33,35,34 ,68,80,50]
+# print("before any operation", my_listt)
+# my_listt.sort()  # it sorts the list in ascending order.
+# print("after operation", my_listt)
 
 # my_list = input("enter the number")
 # print("before any operation", my_list)
@@ -12,9 +12,12 @@
 #     print("hello world",i)
 
 
-def inter_name():
-      i=input("enter your name")
-      for i in range(1,8):
-             print(i)
-        
-inter_name()
+# def inter_name():
+rows = 5
+
+for i in range(1, rows + 1):       # Outer loop → row control
+    for j in range(1, i + 1):       # Inner loop → stars
+        print("*", end="")
+    print()                         # नई लाइन
+
+# inter_name()
